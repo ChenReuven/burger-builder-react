00e909d7e070725c46a9da85c428c313896c57f5
Chen Reuven <Chen.Reuven@emc.com>
2020-03-31 10:17:44 +0300

    add layout component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import Layout from "./components/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout />
       </div>
     );
   }
---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout = props => (
+    <div>Hello World</div>
+);
+
+export default Layout;
