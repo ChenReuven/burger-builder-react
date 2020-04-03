6330e6fb37f8a4e5f14bb13cd75eeba9d1842458
chenreuven <chen.rvn@gmail.com>
2020-04-03 12:48:38 +0300

    add logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = props => (
     <Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar></Toolbar>
+        {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
         <main className={classes.content}>
             {props.children}
         </main>
